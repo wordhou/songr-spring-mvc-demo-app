ec6e326b60b590c64d086e4a23eaeb9fd320a712
Ed <houedward@gmail.com>
2021-04-05 15:32:38 -0700

    created album class and declared fields, auto generate constructor and getters

---
+++ src/main/java/com/edhou/songr/models/Album.java
@@ -0,0 +1,37 @@
+package com.edhou.songr.models;
+
+public class Album {
+    String title;
+    String artist;
+    int songCount;
+    int length;
+    String imageUrl;
+
+    public Album(String title, String artist, int songCount, int length, String imageUrl) {
+        this.title = title;
+        this.artist = artist;
+        this.songCount = songCount;
+        this.length = length;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public int getSongCount() {
+        return songCount;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+}
